9be81083355ef698266b2a754bd376fde197644c
AkashBalani <balani.a@husky.neu.edu>
2024-02-02 18:39:20 -0500

    Add path for find recipes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HealthComponent } from './health/health.component';
 import { ReadyComponent } from './ready/ready.component';
 import { IngredientFormComponent } from './ingredient-form/ingredient-form.component';
+import { FindRecipesComponent } from './find-recipes/find-recipes.component';
 
 const routes: Routes = [
   { path: 'health', component: HealthComponent },
   { path: 'ready', component: ReadyComponent },
   { path: 'ingredient-form', component: IngredientFormComponent },
+  { path: 'find-recipes', component: FindRecipesComponent },
   { path: '', redirectTo: '/ingredient-form', pathMatch: 'full'},
 ];
 
