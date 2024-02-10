5c763e10060b2d9e7d4199949bc651235170b024
AkashBalani <balani.a@husky.neu.edu>
2024-02-10 17:51:20 -0500

    Add express to the project to communicate with kafka

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const kafka = require('kafka-node');
+
+const app = express();
+const Producer = kafka.Producer;
+const client = new kafka.KafkaClient({kafkaHost: 'localhost:9092'});
+const producer = new Producer(client);
+
+app.use(express.json());
+
+app.post('/send', (req, res) => {
+    const payloads = [
+        { topic: 'test', messages: JSON.stringify(req.body), partition: 0 }
+    ];
+    producer.send(payloads, (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Failed to send message' });
+        } else {
+            res.status(200).json(data);
+        }
+    });
+});
+
+app.listen(3000, () => console.log('Listening on port 3000'));
