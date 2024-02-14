3bf2d08201eecd10202641beb424c79c54f0ca50
AkashBalani <balani.a@husky.neu.edu>
2024-02-14 02:33:34 -0500

    Changed kafka host url #2

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 const Producer = kafka.Producer;
-const client = new kafka.KafkaClient({kafkaHost: 'kafka-service.default.svc.cluster.local:9092'});
+const client = new kafka.KafkaClient({kafkaHost: 'kafka-service:9092'});
 const producer = new Producer(client);
 
 app.use(express.json());
