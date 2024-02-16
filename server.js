b0fdd792fe877b6b67c877907a8aba0579f98f07
AkashBalani <balani.a@husky.neu.edu>
2024-02-16 09:18:00 -0500

    Create rabbitmq endpoint instead of kafka

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const kafka = require('kafka-node');
+const amqp = require('amqplib/callback_api');
 
 const cors = require('cors');
 
@@ -7,23 +8,53 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
-const Producer = kafka.Producer;
-const client = new kafka.KafkaClient({kafkaHost: 'kafka-service.app:9093'});
-const producer = new Producer(client);
+app.use(express.json());
+
+const AMQP_URL = 'amqp://rabbitmq-service.app:5672';
+// const Producer = kafka.Producer;
+// const client = new kafka.KafkaClient({kafkaHost: 'kafka-service.app:9093'});
+// const producer = new Producer(client);
 
 app.use(express.json());
 
+// app.post('/django/api/ingredients/', (req, res) => {
+//     const payloads = [
+//         { topic: 'test', messages: JSON.stringify(req.body), partition: 0 }
+//     ];
+//     producer.send(payloads, (err, data) => {
+//         if (err) {
+//             console.error(err);
+//             res.status(500).json({ error: 'Failed to send message' });
+//         } else {
+//             res.status(200).json(data);
+//         }
+//     });
+// });
+
 app.post('/django/api/ingredients/', (req, res) => {
-    const payloads = [
-        { topic: 'test', messages: JSON.stringify(req.body), partition: 0 }
-    ];
-    producer.send(payloads, (err, data) => {
-        if (err) {
-            console.error(err);
-            res.status(500).json({ error: 'Failed to send message' });
-        } else {
-            res.status(200).json(data);
+    amqp.connect(AMQP_URL, (error0, connection) => {
+        if (error0) {
+            console.error(error0);
+            return res.status(500).json({ error: 'Failed to connect to RabbitMQ' });
         }
+        connection.createChannel((error1, channel) => {
+            if (error1) {
+                console.error(error1);
+                return res.status(500).json({ error: 'Failed to create channel' });
+            }
+            const queue = 'test';
+
+            channel.assertQueue(queue, {
+                durable: false
+            });
+
+            const message = JSON.stringify(req.body);
+
+            channel.sendToQueue(queue, Buffer.from(message));
+
+            console.log(" [x] Sent %s", message);
+            res.status(200).json({ message: 'Message sent to RabbitMQ' });
+        });
     });
 });
 
